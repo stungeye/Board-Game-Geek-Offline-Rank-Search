fa4d00545c1f5bae2b70c28e9459d33e011f6fe2
Wally Glutton <stungeye@gmail.com>
2015-02-23 13:45:32 -0600

    Added Ratchet styling.

--- index.php
+++ index.php
@@ -4,20 +4,30 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <link rel="icon" sizes="180x180" href="apple-touch-icon.png">
     <meta name="msapplication-TileColor" content="#ffc40d">
     <meta name="msapplication-TileImage" content="mstile.png">
+    <link rel="icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="stylesheet" href="ratchet.min.css">    
     <title>BoardRank</title>
 </head>
 <body>
-    <form id="search">
-        <input name="query" id="query">
-        <input type="submit" id="search_button" value="search">
-    </form>
-    <div id="debug"></div>
-    <div id="search_results">
-        <p>Add To Homescreen For Offline Use: <a target="_blank" href="http://www.howtogeek.com/196087/how-to-add-websites-to-the-home-screen-on-any-smartphone-or-tablet/">Instructions</a></p>
-        <p>Use once via homescreen while online before using it offline.</p>
+    <header class="bar bar-nav">
+      <h1 class="title">BoardRank</h1>
+    </header>
+
+    <div class="bar bar-standard bar-header-secondary">
+        <form id="search_form">
+            <input type="search" name="query" id="query" placeholder="Search">
+        </form>
+    </div>
+
+    <div class="content">
+        <div id="install_instructions">
+            <p class="content-padded"><a target="_blank" href="http://www.howtogeek.com/196087/how-to-add-websites-to-the-home-screen-on-any-smartphone-or-tablet/">Add to Homescreen for Offline use.</a> Use homescreen app once online before using it offline.</p>
+        </div>
+        <div id="debug"></div>
+        <div id="search_results">
+        </div>
     </div>
     <script type="text/javascript" src="jzed.js"></script>
     <script type="text/javascript" src="main.js"></script>
--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 $boot(function(){
-    var search_button_el = $id('search_button'),
-        search_query_el  = $id('query'),
-        search_results_el = $id('search_results');
+    var search_form_el = $id('search_form'),
+        search_query_el  = $id('query');
 
     if (("standalone" in window.navigator) && window.navigator.standalone) {
-        $html(search_results_el, '');
+        remove_install_instructions();
     }
     
-    $on(search_button_el, 'click', function(event) {
+    $on(search_form_el, 'submit', function(event) {
         event.preventDefault();
+        remove_install_instructions();
        
         var query = search_query_el.value,
             found_games = find_game_by_name(query, board_games);
@@ -17,17 +17,30 @@ $boot(function(){
     });
 });
 
+function remove_install_instructions() {
+    $html($id('install_instructions'), '');
+}
+
 
 function display_games(found_games) {
     var search_results_el = $id('search_results');
     $html(search_results_el, '');
+    var games_ul_el = $new('ul', 'results', '');
+    $style([games_ul_el, 'table-view']);
+    $append(search_results_el, games_ul_el);
     $each(found_games, function(game) {
-        var game_div = $new('div', false, '');
-        $append(game_div, $new('h2', false, game[0]));
-        $append(game_div, $new('p', false, 'Rank: ' + game[1]));
-        $append(game_div, $new('p', false, 'Geek Rating: ' + game[2]));
-        $append(game_div, $new('p', false, 'Number of Voters: ' +  game[3]));
-        $append(search_results_el, game_div);
+        var game_li = $new('li', false, '');
+        $style([game_li, 'table-view-cell']);
+        $append(game_li, $new('h2', false, game[0]));
+        if (game[1] == 0) {
+            $append(game_li, $new('p', false, 'Rank: No BoardGameGeek Ranking'));
+            $append(game_li, $new('p', false, 'Average Rating: ' + game[2]));
+        } else {
+            $append(game_li, $new('p', false, 'Rank: ' + game[1]));
+            $append(game_li, $new('p', false, 'Geek Rating: ' + game[2]));
+        }
+        $append(game_li, $new('p', false, 'Number of Voters: ' +  game[3]));
+        $append(games_ul_el, game_li);
     });
 }
 
